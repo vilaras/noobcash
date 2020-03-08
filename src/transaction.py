d4a9ee560533ba778a6ad4222272538c1eba2262
vilaras <xanthopoulos.vassilis@gmail.com>
2020-03-08 19:00:03 +0200

    Wallet and Transaction initial implementation

--- src/transaction.py
+++ src/transaction.py
@@ -14,30 +14,27 @@ from flask import Flask, jsonify, request, render_template
 
 class Transaction:
 
-    def __init__(self, sender_address, sender_private_key, recipient_address, value):
+    '''
+    Params:
+        sender_address: To public key του wallet από το οποίο προέρχονται τα χρήματα
+        receiver_address: To public key του wallet στο οποίο θα καταλήξουν τα χρήματα
+        amount: το ποσό που θα μεταφερθεί
+        transaction_id: το hash του transaction
+        transaction_inputs: λίστα από Transaction Input 
+        transaction_outputs: λίστα από Transaction Output 
+        Signature
+    '''
+    def __init__(self, sender_address, sender_private_key, receiver_address, amount):
 
-
-        ##set
-
-
-
-        #self.sender_address: To public key του wallet από το οποίο προέρχονται τα χρήματα
-        #self.receiver_address: To public key του wallet στο οποίο θα καταλήξουν τα χρήματα
-        #self.amount: το ποσό που θα μεταφερθεί
-        #self.transaction_id: το hash του transaction
-        #self.transaction_inputs: λίστα από Transaction Input 
-        #self.transaction_outputs: λίστα από Transaction Output 
-        #selfSignature
-
-
+        self.sender_address = sender_address
+        self.receiver_address = receiver_address
+        self.amount = amount
+        # self.transaction_id = transaction_id
+        # self.transaction_inputs = transaction_inputs
+        # self.transaction_outputs = transaction_outputs
     
 
-
-    def to_dict(self):
-        
-
-    def sign_transaction(self):
-        """
-        Sign transaction with private key
-        """
-       
+    # def sign_transaction(self):
+    #     """
+    #     Sign transaction with private key
+    #     """
--- src/wallet.py
+++ src/wallet.py
@@ -12,17 +12,31 @@ from time import time
 from urllib.parse import urlparse
 from uuid import uuid4
 
+from transaction import Transaction
+
+class Wallet:
+
+	def __init__(self):
+		self.public_key, self.private_key = self.keys()
+		self.address = self.public_key
+		self.transactions = []
+
+	def balance(self):
+		return sum(transaction.amount for transaction in self.transactions)
+
+	'''
+	Returns private, public key pair in HEX form
+	'''
+	def keys(self):
+		random_gen = Crypto.Random.new().read
+		priv = RSA.generate(1024, random_gen)
+		pub = priv.publickey()
+
+		return binascii.hexlify(priv.exportKey(format='DER')).decode('ascii'), binascii.hexlify(pub.exportKey(format='DER')).decode('ascii')
 
 
-class wallet:
+# w = Wallet()
+# for i in range(10):
+# 	w.transactions.append(Transaction(1, 1, 1, 1))
 
-	def __init__():
-		##set
-
-		#self.public_key
-		#self.private_key
-		#self_address
-		#self.transactions
-
-	def balance():
-
+# print(w.balance())
